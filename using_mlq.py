8547b049942550c8ae87ccb0dccdcb84a5576eaf
TalhaUsuf <47290815+TalhaUsuf@users.noreply.github.com>
2021-11-01 01:51:23 +0500

    app with redis queues MLQ

--- using_mlq.py
+++ using_mlq.py
@@ -5,18 +5,108 @@
 
 import time
 from mlq.queue import MLQ
+from tqdm import trange
+from fastapi import FastAPI, status
+from fastapi.responses import RedirectResponse
+import uvicorn
 
+desc = """
+
+## How it works
+
+See the figure in readme
+
+"""
+
+app = FastAPI(
+                title="MLQ app",
+                description=desc,
+                docs_url="/documentation"
+            )
 
 
 
 # Create MLQ: namespace, redis host, redis port, redis db
 mlq = MLQ('inference_app', 'localhost', 6379, 0)
 
-job_id = mlq.post({'images': 1234})
+IDS = None
+RESULTS = None
 
-result = None
-while not result:
-    time.sleep(0.1)
-    job = mlq.get_job(job_id)
-    result = job['result']
-    print(result)
+@app.get("/", response_class=RedirectResponse, status_code=status.HTTP_302_FOUND)
+async def docs():
+    return "/documentation"
+
+
+@app.get("/results/", status_code=status.HTTP_200_OK)
+async def get_result():
+    global IDS, RESULTS
+
+    results_ = []
+    # get global variable
+    for k in trange(len(IDS), desc="GETTING result", colour="yellow"):
+    # loop over the jod IDs list and retreive results one by one
+        
+        # against each of the ID ---> acquire results like this
+        result = None
+        while not result:
+            time.sleep(0.1)
+            job = mlq.get_job(IDS[k])
+            # retreive results in FIFO order , if ist item's results have not been calculated then result var. will be None
+            result = job['result'] # if not calculated then result will be None
+            
+
+        results_.append(result)
+
+    RESULTS = results_
+
+    print(RESULTS)
+
+    return {
+                "results" : RESULTS,
+            }
+
+
+
+
+@app.get("/get_result/{id}", status_code=status.HTTP_200_OK)
+async def get_result(id:str):
+    global IDS, RESULTS
+
+  
+    # against each of the ID ---> acquire results like this
+    result = None
+    while not result:
+        time.sleep(0.1)
+        job = mlq.get_job(id)
+        # retreive results in FIFO order , if ist item's results have not been calculated then result var. will be None
+        result = job['result'] # if not calculated then result will be None
+
+    return {
+                "results" : result,
+            }
+
+
+@app.post("/inference/{n_imgs}", status_code=status.HTTP_200_OK)
+async def get_inference(n_imgs : int):
+    global IDS
+    ids = []
+    for k in trange(100, total=100, desc="IMG. NO:", colour="green"):
+
+        job_id = mlq.post({'images': 1234})
+
+        ids.append(job_id)
+
+    # set global variable
+    IDS = ids
+
+    print(IDS)
+
+    return {'msg': 'Processing, check back soon.', 'job_id': IDS}
+
+
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run("using_mlq:app", host="0.0.0.0", port=8081, reload=True, debug=True)
